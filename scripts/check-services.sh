25b00671d2dfb31405ef457db1dc7e054ada2279
dantj8 <dantj8@localhost.localdomain>
2022-02-12 11:47:17 -0300

    verifica lista de servicios iniciado

--- scripts/check-services.sh
+++ scripts/check-services.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "script para verificar los servicios web y conecciones funcionales"
+echo "script para verificar los servicios web y conexiones funcionales"
 
 servicios=('apache' 'nginx' 'docker' 'postgresql' 'mysqld')
 
 for servicio in ${servicios[@]}
   do
   if [[ `ps -acx | grep ${servicio} | wc -l` > 0 ]]; then
-    echo "[+] Servicio de $servicio funcional"
+    echo "[+] Servicio $servicio funcional"
   else
-    echo "[-] $servicio no iniciado - revise" 
+    echo "[-] $servicio no iniciado " 
   fi
 done
